5113718bad6e677e36fcaa0d6f156929760b39ad
Derek Smart <derek@grindaga.com>
2020-02-12 11:43:24 -0500

    refactor out dart from character file
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/characters/Character.go
+++ internal/app/characters/Character.go
@@ -14,17 +14,3 @@ type Attribute struct {
 	Bytes int
 	Reversed bool
 }
-
-func Dart() Character {
-	return Character{
-		HP: 		Attribute{0x534, 2,true},
-		Weapon:		Attribute{0x540, 1,false},
-		Helmet:    	Attribute{0x5411,1,false},
-		Chest:     	Attribute{0x5421,1,false},
-		Boots:     	Attribute{0x5431,1,false},
-		Accessory: 	Attribute{0x5441,1,false},
-	}
-}
-
-
-
---
+++ internal/app/characters/Dart.go
@@ -0,0 +1,12 @@
+package characters
+
+func Dart() Character {
+	return Character{
+		HP: 		Attribute{0x534,2,true},
+		Weapon:		Attribute{0x540,1,false},
+		Helmet:    	Attribute{0x541,1,false},
+		Chest:     	Attribute{0x542,1,false},
+		Boots:     	Attribute{0x543,1,false},
+		Accessory: 	Attribute{0x544,1,false},
+	}
+}
