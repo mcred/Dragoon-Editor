db70a44fa8d37508eb4e3c35e973e084684ca904
Derek Smart <derek@grindaga.com>
2020-02-12 22:55:53 -0500

    full inventory mapping
    Signed-off-by: Derek Smart <derek@grindaga.com>

---
+++ internal/app/inventory/Boots.go
@@ -0,0 +1,16 @@
+package inventory
+
+func Boots() Inventory {
+	return []Item{
+		{93,"Leather Boots"},
+		{94,"Iron Kneepiece"},
+		{95,"Combat Shoes"},
+		{96,"Leather Shoes"},
+		{97,"Soft Boots"},
+		{98,"Stardust Boots"},
+		{99,"Magical Greaves"},
+		{100,"Dancer's Shoes"},
+		{101,"Bandit's Shoes"},
+		//{102,"??"},
+	}
+}
