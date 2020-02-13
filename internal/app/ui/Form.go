3092f5dc3e62a32549d2bb5b3e3a9d255a217788
Derek Smart <derek@grindaga.com>
2020-02-13 11:12:21 -0500

    clean up form creation
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/ui/Form.go
+++ internal/app/ui/Form.go
@@ -10,32 +10,19 @@ import (
 	"fyne.io/fyne/widget"
 )
 
+func createSelect(i inventory.Inventory, a characters.Attribute, s *storage.Slot) *widget.Select {
+	r := widget.NewSelect(i.GetVals(), func(v string) {
+		s.SetValueAtLocation(a, i.GetIDByVal(v))
+	})
+	r.SetSelected(i.GetValByID(s.GetValueByAttribute(a)))
+	return r
+}
+
 func CharacterForm(slot *storage.Slot, card *storage.Card, w fyne.Window) fyne.Widget {
 	dart := characters.Dart()
 
 	name := widget.NewEntry()
 	name.SetPlaceHolder("Dart")
-
-	dartWeapon := widget.NewSelect(inventory.Weapons().GetVals(), func(v string) {
-		slot.SetValueAtLocation(dart.Weapon, inventory.Weapons().GetIDByVal(v))
-	})
-	dartWeapon.SetSelected(inventory.Weapons().GetValByID(slot.GetValueByAttribute(dart.Weapon)))
-	dartArmor := widget.NewSelect(inventory.Armor().GetVals(), func(v string) {
-		slot.SetValueAtLocation(dart.Chest, inventory.Armor().GetIDByVal(v))
-	})
-	dartArmor.SetSelected(inventory.Armor().GetValByID(slot.GetValueByAttribute(dart.Chest)))
-	dartHelmet := widget.NewSelect(inventory.Helms().GetVals(), func(v string) {
-		slot.SetValueAtLocation(dart.Helmet, inventory.Helms().GetIDByVal(v))
-	})
-	dartHelmet.SetSelected(inventory.Helms().GetValByID(slot.GetValueByAttribute(dart.Helmet)))
-	dartBoots := widget.NewSelect(inventory.Boots().GetVals(), func(v string) {
-		slot.SetValueAtLocation(dart.Boots, inventory.Boots().GetIDByVal(v))
-	})
-	dartBoots.SetSelected(inventory.Boots().GetValByID(slot.GetValueByAttribute(dart.Boots)))
-	dartAcc := widget.NewSelect(inventory.Accessories().GetVals(), func(v string) {
-		slot.SetValueAtLocation(dart.Accessory, inventory.Accessories().GetIDByVal(v))
-	})
-	dartAcc.SetSelected(inventory.Accessories().GetValByID(slot.GetValueByAttribute(dart.Accessory)))
 
 	form := &widget.Form{
 		OnCancel: func() {
@@ -47,10 +34,10 @@ func CharacterForm(slot *storage.Slot, card *storage.Card, w fyne.Window) fyne.W
 		},
 	}
 	form.Append("Name", name)
-	form.Append("Weapon", dartWeapon)
-	form.Append("Armor", dartArmor)
-	form.Append("Headgear", dartHelmet)
-	form.Append("Boots", dartBoots)
-	form.Append("Accessories", dartAcc)
+	form.Append("Weapon", createSelect(inventory.Weapons(), dart.Weapon, slot))
+	form.Append("Armor", createSelect(inventory.Armor(), dart.Chest, slot))
+	form.Append("Headgear", createSelect(inventory.Helms(), dart.Helmet, slot))
+	form.Append("Boots", createSelect(inventory.Boots(), dart.Boots, slot))
+	form.Append("Accessories", createSelect(inventory.Accessories(), dart.Accessory, slot))
 	return form
 }
