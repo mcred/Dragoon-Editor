1f96ff57afa2973968bddaddb2cd586dd991ac85
Derek Smart <derek@grindaga.com>
2020-02-13 01:00:21 -0500

    can edit all dart equipment
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/ui/Form.go
+++ internal/app/ui/Form.go
@@ -14,10 +14,27 @@ func CharacterForm(slot *storage.Slot, card *storage.Card) fyne.Widget {
 
 	name := widget.NewEntry()
 	name.SetPlaceHolder("Dart")
-	weapon := widget.NewSelect(inventory.Weapons().GetVals(), func(v string) {
+
+	dartWeapon := widget.NewSelect(inventory.Weapons().GetVals(), func(v string) {
 		slot.SetValueAtLocation(dart.Weapon, inventory.Weapons().GetIDByVal(v))
 	})
-	weapon.SetSelected(inventory.Weapons().GetValByID(slot.GetValueByAttribute(dart.Weapon)))
+	dartWeapon.SetSelected(inventory.Weapons().GetValByID(slot.GetValueByAttribute(dart.Weapon)))
+	dartArmor := widget.NewSelect(inventory.Armor().GetVals(), func(v string) {
+		slot.SetValueAtLocation(dart.Chest, inventory.Armor().GetIDByVal(v))
+	})
+	dartArmor.SetSelected(inventory.Armor().GetValByID(slot.GetValueByAttribute(dart.Chest)))
+	dartHelmet := widget.NewSelect(inventory.Helms().GetVals(), func(v string) {
+		slot.SetValueAtLocation(dart.Helmet, inventory.Helms().GetIDByVal(v))
+	})
+	dartHelmet.SetSelected(inventory.Helms().GetValByID(slot.GetValueByAttribute(dart.Helmet)))
+	dartBoots := widget.NewSelect(inventory.Boots().GetVals(), func(v string) {
+		slot.SetValueAtLocation(dart.Boots, inventory.Boots().GetIDByVal(v))
+	})
+	dartBoots.SetSelected(inventory.Boots().GetValByID(slot.GetValueByAttribute(dart.Boots)))
+	dartAcc := widget.NewSelect(inventory.Accessories().GetVals(), func(v string) {
+		slot.SetValueAtLocation(dart.Accessory, inventory.Accessories().GetIDByVal(v))
+	})
+	dartAcc.SetSelected(inventory.Accessories().GetValByID(slot.GetValueByAttribute(dart.Accessory)))
 
 	form := &widget.Form{
 		OnCancel: func() {
@@ -29,6 +46,10 @@ func CharacterForm(slot *storage.Slot, card *storage.Card) fyne.Widget {
 		},
 	}
 	form.Append("Name", name)
-	form.Append("Weapon", weapon)
+	form.Append("Weapon", dartWeapon)
+	form.Append("Armor", dartArmor)
+	form.Append("Headgear", dartHelmet)
+	form.Append("Boots", dartBoots)
+	form.Append("Accessories", dartAcc)
 	return form
 }
