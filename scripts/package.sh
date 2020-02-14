2a68013603fdc9da5ac8bdee69bbf16f5c3c7169
Derek Smart <derek@grindaga.com>
2020-02-13 19:22:52 -0500

    test
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-export PATH=$PATH:`go env GOPATH`/bin
+export PATH=$PATH:$(go env GOPATH)/bin
 fyne package -executable ./build/LODeditor -os darwin -icon ./assets/icon.png
+zip ${PWD}/LODeditor.zip LODeditor.app
