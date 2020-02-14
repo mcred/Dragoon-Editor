93e2b1d57c09410aa0507ca6996ee53f927f16ee
Derek Smart <derek@grindaga.com>
2020-02-13 20:31:24 -0500

    split weapons into user catagories
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/inventory/Weapons.go
+++ internal/app/inventory/Weapons.go
@@ -1,6 +1,6 @@
 package inventory
 
-func Weapons() Inventory {
+func Swords() Inventory {
 	return []Item {
 		{0,"Broad Sword"},
 		{1,"Bastard Sword"},
@@ -10,11 +10,21 @@ func Weapons() Inventory {
 		{5,"Fairy Sword"},
 		{6,"Claymore"},
 		{7,"Soul Eater"},
+	}
+}
+
+func Axes() Inventory {
+	return []Item {
 		{8,"Axe"},
 		{9,"Tomahawk"},
 		{10,"Battle Axe"},
 		{11,"Great Axe"},
 		{12,"Indora's Axe"},
+	}
+}
+
+func Daggers() Inventory {
+	return []Item{
 		{13,"Rapier"},
 		{19,"Demon Stiletto"},
 		{14,"Shadow Cutter"},
@@ -22,6 +32,11 @@ func Weapons() Inventory {
 		{16,"Flamberge"},
 		{17,"Gladius"},
 		{18,"Dragon Buster"},
+	}
+}
+
+func Spears() Inventory {
+	return []Item {
 		{20,"Spear"},
 		{21,"Lance"},
 		{26,"Twister Glaive"},
@@ -29,6 +44,11 @@ func Weapons() Inventory {
 		{23,"Spear Of Terror"},
 		{24,"Partisan"},
 		{25,"Halberd"},
+	}
+}
+
+func Bows() Inventory {
+	return []Item {
 		{27,"Short Bow"},
 		{28,"Sparkle Arrow"},
 		{29,"Long Bow"},
@@ -36,12 +56,22 @@ func Weapons() Inventory {
 		{31,"Virulent Arrow"},
 		{33,"Arrow Of Force"},
 		{32,"Detonate Arrow"},
+	}
+}
+
+func Maces() Inventory {
+	return []Item {
 		{34,"Mace"},
 		{35,"Morning Star"},
 		{36,"War Hammer"},
 		{37,"Heavy Mace"},
 		{39,"Pretty Hammer"},
 		{38,"Basher"},
+	}
+}
+
+func Knuckles() Inventory {
+	return []Item {
 		{40,"Iron Knuckle"},
 		{41,"Beast Fang"},
 		{42,"Diamond Claw"},
--- scripts/package.sh
+++ scripts/package.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-export PATH=$PATH:$(go env GOPATH)/bin
+#export PATH=$PATH:$(go env GOPATH)/bin
 fyne package -executable ./build/LODeditor -os darwin -icon ./assets/icon.png
