86b59b4f7882aff4e7672f0aab44e93e23034468
Derek Smart <derek@grindaga.com>
2020-02-13 20:20:35 -0500

    fix bug with shana and lavitz
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- internal/app/characters/Shana.go
+++ internal/app/characters/Shana.go
@@ -2,12 +2,12 @@ package characters
 
 func Shana() Character {
 	return Character{
-		XP:			Attribute{0x584,4,true},
-		HP: 		Attribute{0x534,2,true}, //TODO map this
-		Weapon:		Attribute{0x56C,1,false},
-		Helmet:    	Attribute{0x56D,1,false},
-		Chest:     	Attribute{0x56E,1,false},
-		Boots:     	Attribute{0x56F,1,false},
-		Accessory: 	Attribute{0x570,1,false},
+		XP:			Attribute{0x558,4,true},
+		HP: 		Attribute{0x560,2,true},
+		Weapon:		Attribute{0x598,1,false},
+		Helmet:    	Attribute{0x599,1,false},
+		Chest:     	Attribute{0x59A,1,false},
+		Boots:     	Attribute{0x59B,1,false},
+		Accessory: 	Attribute{0x59C,1,false},
 	}
 }
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+set -ex
+
+export PATH=$PATH:$(go env GOPATH)/bin
 fyne package -executable ./build/LODeditor -os darwin -icon ./assets/icon.png
